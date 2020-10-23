7c37002ed0c53584b0f603faacbb34e287e54d45
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-10-22 20:00:18 -0700

    e

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ app.use(express.json({limit: '80mb'}));
 // app.use(express.static("client/public"));
 app.use(express.static("client/build"));
 
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 // Add routes, both API and view
 app.use(cors());
