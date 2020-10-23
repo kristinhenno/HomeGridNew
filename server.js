844c3911cfe7132b00efcf64fe85f2ed615cc7b6
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-10-22 23:04:38 -0700

    typo

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ connection.once("open", () => {
 });
 
 app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
+  let url = path.join(__dirname, '/client/build', 'index.html');
   if (!url.startsWith('/app/')) // since we're on local windows
     url = url.substring(1);
   res.sendFile(url);
