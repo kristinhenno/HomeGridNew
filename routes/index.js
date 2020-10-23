ec84fec0b37404bf0dafb5934aa4673be5ed51be
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-10-22 20:16:16 -0700

    d

--- routes/index.js
+++ routes/index.js
@@ -18,9 +18,14 @@ router.use("/stripe", stripeRoute);
 router.use("/scrape", scrapeRoute);
 
 
-// If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
+
+router.get('*', function (req, res) {
+  const index = path.join(__dirname, 'client', 'build', 'index.html');
+  res.sendFile(index);
 });
 
 module.exports = router;
