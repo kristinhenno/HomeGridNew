a00d95925a9318b6c45306a49989a05ecc1b8e7c
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-10-23 23:22:49 -0700

    center google

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@
 const path = require("path");
 const router = require("express").Router();
 
-const itemsRouter = require("./item");
-const usersRouter = require("./users");
+const itemsRouter = require("/item");
+const usersRouter = require("/users");
 const yelpRoute = require("./api/yelp");
 const stockRoute = require("./api/stocks");
-const stripeRoute = require("./stripe");
+const stripeRoute = require("/stripe");
 const scrapeRoute = require("./api/scrape");
 
 
