9ce4fc4f86e62eaf0e2fb55814bd06095084ed64
Kristin Henno <kristinhenno@Kristins-MacBook-Pro.local>
2020-10-23 18:27:17 -0700

    D

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,8 @@ router.use("/scrape", scrapeRoute);
 
 
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+    // res.sendFile(path.join(__dirname, "../Client/build/index.html"));
+    res.sendFile(path.join(__dirname, '../Client','public', 'index.html'));
   });
   
 
